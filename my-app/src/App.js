80d8ad6c812070955f90a413230bd458f03e9362
Katherine Jin <dianjin@fb.com>
2020-12-14 20:57:53 -0500

    idk

---
+++ my-app/src/App.js
@@ -0,0 +1,90 @@
+import logo from './logo.svg';
+import './App.css';
+import html2canvas from 'html2canvas';
+import React, { useState, useEffect } from 'react';
+
+// My components
+import HeadlineBox from './what-headline.js'
+import Words from './words/words.js'
+
+function saveCanvases() {
+  var canvases = document.querySelectorAll("canvas");
+    function downloadCanvas(canvas) {
+    // Convert the canvas to data
+    var image = canvas.toDataURL();
+    // Create a link
+    var aDownloadLink = document.createElement('a');
+    // Add the name of the file to the link
+    aDownloadLink.download = 'canvas_image.png';
+    // Attach the data to the link
+    aDownloadLink.href = image;
+    // Get the code to click the download link
+    aDownloadLink.click();
+  }
+  canvases.forEach(downloadCanvas);
+
+}
+
+function App() {
+  useEffect(() => {
+    // Update the document title using the browser API
+    console.log("page loaded")
+    var canvases = document.querySelectorAll(".container");
+
+    function appendCanvas(canvas) {
+      html2canvas(canvas).then(canvas => {
+        console.log("canvas")
+        document.querySelector(".canvas").appendChild(canvas)
+      });
+    }
+
+    canvases.forEach(appendCanvas);
+
+  });
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div class="container">
+          <Words
+            simplified="共产主义"
+            traditional="共產主義"
+            pinyin="gòng chǎn zhǔ yì"
+            trans="communism"
+          />
+          <Words
+            simplified="共产主"
+            traditional="共產主"
+            pinyin="gòng chǎn zhǔ"
+            trans="communism"
+          />
+          <Words
+            simplified="共产"
+            traditional="共產"
+            pinyin="gòng chǎn"
+            trans="communism"
+          />
+          {/* <HeadlineBox/> */}
+        </div>
+        <button onClick={saveCanvases}>
+        download images
+        </button>
+        <div class="canvas">
+        </div>
+        {/* <p>
+          lol <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/what-headline.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import './headline.css';
+
+function HeadlineBox() {
+  return (
+    <div className="box">
+        What headline are you?
+    </div>
+  );
+}
+
+export default HeadlineBox;
